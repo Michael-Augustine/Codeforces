06726edb2c097609eb9a369cf721aea7f7b99ac9
Michael Augustine <88915387+Michael-Augustine@users.noreply.github.com>
2021-08-19 10:15:02 +0530

    Updated

---
+++ A-Brick/A-Brick/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Brick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N =Convert.ToInt32(Console.ReadLine());
+            int W =Convert.ToInt32(Console.ReadLine());
+            if (1 <= N && W <= 1000)
+            {
+                int B = N / W;
+                Console.WriteLine(B);
+            }
+                       
+        }
+    }
+}
