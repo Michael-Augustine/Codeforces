e41f1f2025f322306cedd0f92ff8a5d79c788d0e
Michael Augustine <pushpamichaeldoss@gmail.com>
2021-09-06 09:37:13 +0530

    BlcakJAck

---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Pink = Console.ReadLine().Split(' ');
+            int A1 = Convert.ToInt32(Pink[0]);
+            int A2 = Convert.ToInt32(Pink[1]);
+            int A3 = Convert.ToInt32(Pink[2]);
+            int result;
+            result = A1 + A2 + A3;
+            if (result<22)
+            {
+                Console.WriteLine("Win");
+            }
+            else 
+            {
+                Console.WriteLine("Bust");
+            }
+            
+        }
+    }
+}
